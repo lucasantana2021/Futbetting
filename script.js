c3afc6256c0a587d4bba7200d7e5c0506914faf3
Lucas Santana <83085301+lucasantana2021@users.noreply.github.com>
2022-07-04 02:54:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function valorapagar(){
+    let total = valor1.valueAsNumber + valor2.valueAsNumber + valor3.valueAsNumber + valor4.valueAsNumber;
+  
+  resposta.textContent = total.toFixed(1)
+  }
